91b2626ded160529d67c6c13c4bd44c7d371308c
Pouyan Norouzi <irfn7pouyan@gmail.com>
2024-03-03 22:48:20 -0800

    fixed bug where navbar did not show and added it to main

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,5 +1,4 @@
 function loadSkeleton() {
-    console.log($('#navbarPlaceholder').load('./text/nav-before-login.html'));
-    console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+    console.log($('#navbarPlaceholder').load('../text/nav_before_login.html'));
 }
 loadSkeleton();
