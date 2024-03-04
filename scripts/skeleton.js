0dba64acfb104f9265cf45ef88ccb6b2749de692
Pouyan Norouzi <irfn7pouyan@gmail.com>
2024-03-03 22:28:12 -0800

    Added navbar as a seprate file and made script to run it

---
+++ scripts/skeleton.js
@@ -0,0 +1,5 @@
+function loadSkeleton() {
+    console.log($('#navbarPlaceholder').load('./text/nav-before-login.html'));
+    console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+}
+loadSkeleton();
