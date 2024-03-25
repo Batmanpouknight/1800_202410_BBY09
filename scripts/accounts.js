154f2c552cf46204ea1744a8033d7616d96502f9
Bdiep6 <bdiep6@my.bcit.ca>
2024-03-24 19:14:55 -0700

    Added accounts.html for layout, accounts.css for styling,  accounts.js to load user info from database

---
+++ scripts/accounts.js
@@ -0,0 +1,14 @@
+function getContactInformation(user) {
+    let accounts = document.getElementById("about");
+    db.collection("users").doc(user).get().then(docUser => {
+        var email = docUser.data().email;
+        var name = docUser.data().name;
+        var school = docUser.data().school;
+        var country = docUser.data().country;
+        document.getElementById("name").innerHTML = "<label>Name</label><p>" + name + "</p>";
+        document.getElementById("email").innerHTML = "<label>Email</label><p>" + email + "</p>";
+        document.getElementById("school").innerHTML = "<label>School</label><p>" + school + "</p>";
+        document.getElementById("country").innerHTML = "<label>Country</label><p>" + country + "</p>";
+    })
+}
+getContactInformation("UmYiMWubRmQ0kCt4aEbiMuknsm12");
