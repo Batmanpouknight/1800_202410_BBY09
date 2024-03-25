0bee19d7dba07a56c26448cbccfbfb222a28aa54
Pouyan Norouzi <irfn7pouyan@gmail.com>
2024-03-24 18:32:45 -0700

    added a way for datails and search page to interact

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,10 @@ function logout() {
 }
 
 function displayProducts(collection) {
-  let productTemplate = document.getElementById("productTemplate");
   db.collection(collection).get()
-    .then(allProducts => {
+    .then(allPListings => {
       var i = 1;
-      allProducts.forEach(doc => {
+      allPListings.forEach(doc => {
         var userId = doc.data().userId;
         db.collection("users").doc(userId).get()
           .then(userDoc => {
@@ -24,61 +23,69 @@ function displayProducts(collection) {
                 var price = doc.data().price;
                 var user = userDoc.data().name;
                 var image = doc.data().image;
-                let newProduct = productTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+                var docId = doc.id;
 
-                //update title and text and image
-                newProduct.querySelector('.number').innerHTML = i;
-                newProduct.querySelector('.product').innerHTML = bookName + "<br>" + "Posted by: " + user;
-                newProduct.querySelector('.date').innerHTML = "Date posted:" + date;
-                newProduct.querySelector('.price').innerHTML = "$" + price;
-                newProduct.querySelector('.image').src = "./images/" + image + ".jpg"; 
-                console.log(newProduct.querySelector('.image').src);
+                let item = document.getElementById('item' + i);
+                item.querySelector('.number').innerHTML = i;
+                item.querySelector('.product').innerHTML = bookName + "<br>" + "Posted by: " + user;
+                item.querySelector('.date').innerHTML = "Date posted:" + date;
+                item.querySelector('.price').innerHTML = "$" + price;
+                item.querySelector('.image').src = "./images/" + image + ".jpg";
+                localStorage.setItem('listingDocID' + i, docId);
 
                 i++;
-                //attach to gallery, Example: "hikes-go-here"
-                document.getElementById(collection + "-go-here").appendChild(newProduct);
               }).catch(error => {
                 console.error("Error getting book document: ", error);
               })
           }).catch(error => {
             console.error("Error getting users document: ", error);
           })
-          
+
       })
     })
 }
-function getListingInformation(listing){
+
+function redirect(row) {
+  var listingId = localStorage.getItem("listingDocID" + row);
+  localStorage.setItem('listingDocID', listingId);
+  window.location.href = 'details.html';
+  console.log(listingId);
+ 
+}
+
+function getListingInformation(listing) {
   let details = document.querySelector(".container");
-  db.collection("listings").doc(listing).get().then(docListing=>{
-      var bookId = docListing.data().bookId;
-      var date = docListing.data().date.toDate().toLocaleString();
-      var description = docListing.data().description;
-      var price = docListing.data().price;
-      var userId = docListing.data().userId;
-      db.collection("books").doc(bookId).get().then(docBook=>{
-        var bookName = docBook.data().name;
-        var author = docBook.data().author;
-        var course = docBook.data().course;
-        var edition = docBook.data().edition;
-        var publisher = docBook.data().publisher;
-        var yearPrinted = docBook.data().yearPrinted;
-        db.collection("users").doc(userId).get().then(docUser=>{
-          var email = docUser.data().email;
-          var name = docUser.data().name;
-          document.querySelector(".product-description").innerHTML = "<span>Textbook</span><h1>" + bookName + "</h1>";
-          document.getElementById("author").innerHTML = "<span>Author: </span>" + author;
-          document.getElementById("edition").innerHTML = "<span>Edition: </span>" + edition;
-          document.getElementById("publisher").innerHTML = "<span>Publisher: </span>" + publisher;
-          document.getElementById("yearPrinted").innerHTML = "<span>Year of Publication: </span>" + yearPrinted;
-          document.getElementById("description").innerHTML ="<p> </p>" + description;
-          document.getElementById("date").innerHTML = "<p>Posted on:</p>" + date;
-          document.getElementById("price").innerHTML = "<span>Price: $</span>" + price;
-          document.getElementById("name").innerHTML = "<p>Name:</p>" + name;
-          document.getElementById("email").innerHTML = "<p>Email:</p>" + email;
-          
-          
-        })
+  db.collection("listings").doc(listing).get().then(docListing => {
+    var bookId = docListing.data().bookId;
+    var date = docListing.data().date.toDate().toLocaleString();
+    var description = docListing.data().description;
+    var price = docListing.data().price;
+    var userId = docListing.data().userId;
+    var image = docListing.data().image;
+    db.collection("books").doc(bookId).get().then(docBook => {
+      var bookName = docBook.data().name;
+      var author = docBook.data().author;
+      var course = docBook.data().course;
+      var edition = docBook.data().edition;
+      var publisher = docBook.data().publisher;
+      var yearPrinted = docBook.data().yearPrinted;
+      db.collection("users").doc(userId).get().then(docUser => {
+        var email = docUser.data().email;
+        var name = docUser.data().name;
+        document.querySelector(".product-description").innerHTML = "<span>Textbook</span><h1>" + bookName + "</h1>";
+        document.getElementById("image").src = "./images/" + image + ".jpg";
+        document.getElementById("author").innerHTML = "<span>Author: </span>" + author;
+        document.getElementById("edition").innerHTML = "<span>Edition: </span>" + edition;
+        document.getElementById("publisher").innerHTML = "<span>Publisher: </span>" + publisher;
+        document.getElementById("yearPrinted").innerHTML = "<span>Year of Publication: </span>" + yearPrinted;
+        document.getElementById("description").innerHTML = "<p> </p>" + description;
+        document.getElementById("date").innerHTML = "<p>Posted on:</p>" + date;
+        document.getElementById("price").innerHTML = "<span>Price: $</span>" + price;
+        document.getElementById("name").innerHTML = "<p>Name:</p>" + name;
+        document.getElementById("email").innerHTML = "<p>Email:</p>" + email;
+
+
       })
+    })
   })
 }
-getListingInformation("AFH4semSM0RTKw4FdaFu");
